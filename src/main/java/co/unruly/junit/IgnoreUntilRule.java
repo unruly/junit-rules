ee34f7dff33a182105bab2833ea960e1315cb4c3
simon.dudley <simon.dudley@autotrader.co.uk>
2018-02-18 17:12:10 +0000

    @IgnoreUntil annotation can now be on a method only. @IgnoreUntil method annotation overrides class annotation when non-default value set. Fixes issue #3

--- src/main/java/co/unruly/junit/IgnoreUntilRule.java
+++ src/main/java/co/unruly/junit/IgnoreUntilRule.java
@@ -10,12 +10,14 @@ import static org.joda.time.format.DateTimeFormat.forPattern;
 
 public class IgnoreUntilRule implements TestRule {
 
+    static final String DEFAULT_IGNORE = "2000-01-01";
+
     @Override
     public Statement apply(Statement base, Description description) {
 
         IgnoreUntil ignoreUntil = description.getAnnotation(IgnoreUntil.class);
 
-        if (ignoreUntil != null && description.getTestClass() != null) {
+        if (ignoreUntil != null && ignoreUntil.value().equals(DEFAULT_IGNORE) && description.getTestClass() != null) {
             ignoreUntil = description.getTestClass().getAnnotation(IgnoreUntil.class);
         }
 
